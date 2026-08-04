7f0f816882fed49db4dab2a16992bdbc28f899b4
Letícia Schütze <the.leticia.schutze@gmail.com>
2026-08-04 18:07:44 -0300

    040826

--- 260728/led_semaforo07/led_semaforo07.ino
+++ 260728/led_semaforo07/led_semaforo07.ino
@@ -1,22 +1,60 @@
 //definindo o led
-int LED_RED = 2;
-int LED_YEL = 3;
-int LED_GRE = 4;
+int r1_gre = 2;
+int r1_yel = 3;
+int r1_red = 4;
+int r2_gre = 5;
+int r2_yel = 6;
+int r2_red = 7;
+int r3_gre = 8;
+int r3_yel = 9;
+int r3_red = 10;
 
 void setup() {
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YEL, OUTPUT); 
-  pinMode(LED_GRE, OUTPUT);
+  pinMode(r1_gre, OUTPUT);
+  pinMode(r1_yel, OUTPUT); 
+  pinMode(r1_red, OUTPUT);
+  pinMode(r2_gre, OUTPUT);
+  pinMode(r2_yel, OUTPUT); 
+  pinMode(r2_red, OUTPUT);
+  pinMode(r3_gre, OUTPUT);
+  pinMode(r3_yel, OUTPUT); 
+  pinMode(r3_red, OUTPUT);
 }
 
 void loop() {
-    
-  digitalWrite (LED_GRE, HIGH); 
-  delay (3000); 
-  digitalWrite (LED_GRE, LOW); 
-  digitalWrite (LED_YEL, HIGH); 
-  delay (1000);
-  digitalWrite (LED_YEL, LOW); 
-  digitalWrite (LED_RED, HIGH); 
-  delay (3000);  
-  digitalWrite (LED_RED, LOW); 
+  //rua 01
+  digitalWrite(r1_gre, HIGH);
+  digitalWrite(r1_yel, LOW);
+  digitalWrite(r1_red, LOW);
+  digitalWrite(r2_gre, LOW);
+  digitalWrite(r2_yel, LOW);
+  digitalWrite(r2_red, HIGH);
+  digitalWrite(r3_gre, LOW);
+  digitalWrite(r3_yel, LOW);
+  digitalWrite(r3_red, HIGH);
+  delay(3000);
+  
+  //transição
+  digitalWrite(r1_gre, LOW);
+  digitalWrite(r1_yel, HIGH);
+  delay(1000);
+  
+  //rua 02 
+  digitalWrite(r1_yel, LOW);
+  digitalWrite(r1_red, HIGH);
+  digitalWrite(r2_red, HIGH);
+  digitalWrite(r2_gre, LOW);
+  delay(3000);
+  
+  //transição
+  digitalWrite(r2_gre, LOW);
+  digitalWrite(r2_yel, HIGH);
+  delay(1000);
+  
+  //rua 03
+  digitalWrite(r2_yel, LOW);
+  digitalWrite(r2_red, HIGH);
+  digitalWrite(r3_red, LOW);
+  digitalWrite(r3_gre, HIGH);
+  delay(3000);
+}
--- 260804/genius09/genius09.ino
+++ 260804/genius09/genius09.ino
@@ -1,20 +1,28 @@
 //definindo o led e botão
-int led[4] = {9, 10}; 
-int S1 = 7;
-int S2 = 6;
+int led[4] = {9, 10, 11, 12}; 
+int botao[4] = {4, 5, 6, 7};
+
+int sequencia[100];
+int rodada;
+
 
 
 void setup() {
-  for(int i=0; i<2; i++){
+  for(int i=0; i<4; i++){ //coloca leds
     pinMode(led[i], OUTPUT);
+  } 
+  for(int i=0; i<4; i++){ //coloca botão
+    pinMode(botao[i], OUTPUT);
   }
-  pinMode(S1, INPUT); //coloca botão
-  pinMode(S2, INPUT);
 }
 
 void loop() {
+  rodada++;
     int ligaum = digitalRead(S1);
     int ligadois = digitalRead(S2);
+
+
+    
     
     if(ligaum == 0){
       digitalWrite (led[0], HIGH); //liga o amarelo
